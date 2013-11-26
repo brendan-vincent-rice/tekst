9c4c6fda7a1ab9a07304e7bb0845932e66efce47
Brendan <hey@gobrendan.com>
2013-11-26 15:10:52 -0500

    add JS, working on plugin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 (function(){
 
-  $('.demo').tekst();
+  //$('.demo').tekst();
+
+  $(window).on("resize", function () {
+  
+    // Variables
+    var container = $.trim($(".demo").text()), 
+        characters = container.split(""),
+        containerWidth = $(".demo").width();
+
+    // Remove container's contents
+    $(".demo").empty();
+
+    // Wrap each character in a span
+    $.each(characters, function(i, el) {
+       $(".demo").append("<span>"+el+"</span");
+    });
+    
+    // How many spans (characters)?
+    var letterCount = $(".demo span").length;
+  
+    // Set span font size    
+    fontSize = containerWidth / letterCount * 1.55 + "px";
+  
+     // Apply CSS change
+    $(".demo span").css({
+      "font-size": fontSize 
+    });
+  
+  }).resize();
 
 }());
